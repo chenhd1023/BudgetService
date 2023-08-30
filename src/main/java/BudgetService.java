d64e045ad740d52ff8d867c54b4168ae658567b8
hd_chen <hd_chen@trendmicro.com>
2023-08-30 14:39:32 +0800

    clean out code

--- src/main/java/BudgetService.java
+++ src/main/java/BudgetService.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -33,37 +34,28 @@ public class BudgetService {
 
         long daysBetween;
         double rtBudget = 0;
-        for (YearMonth yearMonth: yearMonthBetween){
+        for (YearMonth yearMonth : yearMonthBetween) {
 
 
-            if (yearMonth.equals(startYearMonth) ) {
-                for (Budget budget : listBudgets) {
-                    if (budget.getYearMonthInstance().equals(startYearMonth)) {
-                        if (end.isAfter(yearMonthBetween.get(0).atEndOfMonth())) {
-                            daysBetween = DAYS.between(start, yearMonthBetween.get(0).atEndOfMonth());
-                        } else {
-                            daysBetween = DAYS.between(start, end);
-                        }
-                        rtBudget += budget.dailyAmount(budget) * (daysBetween + 1);
-                    }
-                }
+            Optional<Budget> findBudget = listBudgets.stream().filter(b -> b.getYearMonthInstance().equals(yearMonth)).findFirst();
+            if (findBudget.isEmpty()) {
+                continue;
             }
-            else if (yearMonth.equals(endYearMonth)  ) {
-                for (Budget budget : listBudgets) {
-                    if (budget.getYearMonthInstance().equals(yearMonth)) {
-                        daysBetween = DAYS.between(yearMonth.atDay(1), end);
-                        rtBudget += budget.dailyAmount(budget) * (daysBetween + 1);
-                    }
+            Budget budget = findBudget.get();
+
+            if (budget.getYearMonthInstance().equals(startYearMonth)) {
+                if (end.isAfter(yearMonthBetween.get(0).atEndOfMonth())) {
+                    daysBetween = DAYS.between(start, yearMonthBetween.get(0).atEndOfMonth());
+                } else {
+                    daysBetween = DAYS.between(start, end);
                 }
+            } else if (budget.getYearMonthInstance().equals(endYearMonth)) {
+                daysBetween = DAYS.between(yearMonth.atDay(1), end);
+            } else {
+                daysBetween = DAYS.between(budget.getYearMonthInstance().atDay(1), budget.getYearMonthInstance().atEndOfMonth());
+
             }
-            else {
-                for (Budget budget : listBudgets) {
-                    if (yearMonth.equals(budget.getYearMonthInstance())) {
-                        daysBetween = DAYS.between(budget.getYearMonthInstance().atDay(1),budget.getYearMonthInstance().atEndOfMonth());
-                        rtBudget += budget.dailyAmount(budget)  *  (daysBetween + 1) ;
-                    }
-                }
-            }
+            rtBudget += budget.dailyAmount(budget) * (daysBetween + 1);
         }
 
         return rtBudget;
